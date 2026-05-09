d625464cc7d69059063a972ffff94ef100aac0ed
maulidyaafriani <maulidyafriani@gmail.com>
2026-05-10 02:49:25 +0700

    7-2

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -3,6 +3,10 @@
 namespace App\Filament\Resources\Products\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Section;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\IconEntry;
 
 class ProductInfolist
 {
@@ -10,7 +14,62 @@ class ProductInfolist
     {
         return $schema
             ->components([
-                //
+                Section::make('Product Info')
+                ->description('')
+                    ->schema([
+                        TextEntry::make('name')
+                            ->label('Product Name')
+                            ->weight('bold')
+                            ->color('primary'),
+                        TextEntry::make('id')
+                            ->label('Product ID'),
+                        TextEntry::make('sku')
+                            ->label('Product SKU')
+                            ->badge()
+                            ->color('info'),
+                        TextEntry::make('description')
+                            ->label('Product Description'),
+                        TextEntry::make('created_at')
+                            ->label('Product Creation Date')
+                            ->date('d M Y')
+                            ->color('info'),
+                    ])
+                    ->columnSpanFull(),
+                Section::make('Pricing & Stock')
+                    ->schema([
+                        TextEntry::make('price')
+                            ->label('Product Price')
+                            ->icon('heroicon-o-currency-dollar')
+                            ->formatStateUsing(fn (string $state): string => 'Rp ' . number_format($state, 0, ',', '.')),
+                        TextEntry::make('stock')
+                            ->label('Product Stock')
+                            ->icon('heroicon-o-circle-stack'),
+                    ])
+                    ->columnSpanFull(),
+                Section::make('Media & Status')
+                    ->schema([
+                        ImageEntry::make('image')
+                            ->label('Product Image')
+                            ->disk('public'),
+                        TextEntry::make('price')
+                            ->label('Product Price')
+                            ->weight('bold')
+                            ->color('primary')
+                            ->icon('heroicon-o-currency-dollar')
+                            ->formatStateUsing(fn (string $state): string => 'Rp ' . number_format($state, 0, ',', '.')),
+                        IconEntry::make('is_active')
+                            ->label('Is Active')
+                            ->boolean(),
+                        IconEntry::make('is_featured')
+                            ->label('Is Featured')
+                            ->boolean(),
+                        TextEntry::make('stock')
+                            ->label('Product Stock')
+                            ->weight('bold')
+                            ->color('primary')
+                            ->icon('heroicon-o-circle-stack'),
+                    ])
+                    ->columnSpanFull(),
             ]);
     }
 }
--- app/Filament/Resources/Products/Tables/ProductsTable.php
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -17,9 +17,13 @@ class ProductsTable
         return $table
             ->columns([
                 TextColumn::make('name'),
-                TextColumn::make('sku'),
-                TextColumn::make('price'),
-                TextColumn::make('stock'),
+                TextColumn::make('sku')
+                    ->badge()
+                    ->color('info'),
+                TextColumn::make('price')
+                    ->formatStateUsing(fn (string $state): string => 'Rp ' . number_format($state, 0, ',', '.')),
+                TextColumn::make('stock')
+                    ->icon('heroicon-o-circle-stack'),
                 ImageColumn::make('image')
                     ->disk('public'),
                 TextColumn::make('is_active')
